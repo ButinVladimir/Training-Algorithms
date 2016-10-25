b6af53e3743f82bbb07ccd96e4fe938a99163d81
Vladimir Butin <vladimir.olre.butin@gmail.com>
2016-10-26 00:24:54 +0300

    Pond problem

---
+++ Algorithms/Fun/Pond.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Fun.Pond
+{
+    class Pond
+    {
+        private class Stuff
+        {
+            public int Index { get; set; }
+
+            public int Level { get; set; }
+
+            public int MinHeight { get; set; }
+
+            public Stuff(int index, int level, int minHeight)
+            {
+                this.Index = index;
+                this.Level = level;
+                this.MinHeight = minHeight;
+            }
+        }
+
+        public static int MaxDepth(int[] heights)
+        {
+            Stack<Stuff> stack = new Stack<Stuff>();
+            Stuff top;
+            int maxHeight = heights[0], maxLocalHeight;
+            int minHeight;
+            int result = 0;
+
+            for (int i = 0; i < heights.Length; i++)
+            {
+                if (stack.Count == 0)
+                {
+                    stack.Push(new Stuff(i, 0, heights[i]));
+                    continue;
+                }
+
+                top = stack.First();
+                if (heights[top.Index] >= heights[i])
+                {
+                    stack.Push(new Stuff(i, 0, heights[i]));
+                }
+                else 
+                {
+                    maxLocalHeight = heights[i];
+                    if (maxHeight < maxLocalHeight)
+                    {
+                        maxLocalHeight = maxHeight;
+                    }
+
+                    minHeight = maxLocalHeight;
+                    while (stack.Count > 0 && heights[top.Index] < maxLocalHeight)
+                    {
+                        if (top.MinHeight < minHeight)
+                        {
+                            minHeight = top.MinHeight;
+                        }
+
+                        stack.Pop();
+                        if (stack.Count > 0)
+                        {
+                            top = stack.First();
+                        }
+                    }
+
+                    if (stack.Count > 0)
+                    {
+                        if (top.MinHeight > minHeight)
+                        {
+                            top.MinHeight = minHeight;
+                        }
+                        if (top.Level < maxLocalHeight)
+                        {
+                            top.Level = maxLocalHeight;
+                        }
+
+                        if (top.Level - top.MinHeight > result)
+                        {
+                            result = top.Level - top.MinHeight;
+                        }
+                    }
+
+                    stack.Push(new Stuff(i, 0, heights[i]));
+                }
+
+                if (heights[i] > maxHeight)
+                {
+                    maxHeight = heights[i];
+                }
+            }
+
+            return result;
+        }
+
+        public static int Volume(int[] heights)
+        {
+            Stack<Stuff> stack = new Stack<Stuff>();
+            Stuff top;
+            int maxHeight = heights[0], maxLocalHeight;
+            int minHeight;
+            int result = 0;
+            int index;
+            int delta;
+
+            for (int i = 0; i < heights.Length; i++)
+            {
+                if (stack.Count == 0)
+                {
+                    stack.Push(new Stuff(i, heights[i], heights[i]));
+                    continue;
+                }
+
+                top = stack.First();
+                if (heights[top.Index] >= heights[i])
+                {
+                    stack.Push(new Stuff(i, heights[i], heights[i]));
+                }
+                else
+                {
+                    maxLocalHeight = heights[i];
+                    if (maxHeight < maxLocalHeight)
+                    {
+                        maxLocalHeight = maxHeight;
+                    }
+
+                    minHeight = maxLocalHeight;
+                    index = i;
+                    delta = 0;
+                    while (stack.Count > 0 && heights[top.Index] < maxLocalHeight)
+                    {
+                        delta += (index - (top.Index)) * (maxLocalHeight - top.Level);
+
+                        if (top.MinHeight < minHeight)
+                        {
+                            minHeight = top.MinHeight;
+                        }
+
+                        stack.Pop();
+                        if (stack.Count > 0)
+                        {
+                            top = stack.First();
+                        }
+                    }
+
+                    if (stack.Count > 0)
+                    {
+                        if (top.MinHeight > minHeight)
+                        {
+                            top.MinHeight = minHeight;
+                        }
+                        if (top.Level < maxLocalHeight)
+                        {
+                            top.Level = maxLocalHeight;
+                        }
+
+                        result += delta;
+                    }
+
+                    stack.Push(new Stuff(i, heights[i], heights[i]));
+                }
+
+                if (heights[i] > maxHeight)
+                {
+                    maxHeight = heights[i];
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MustReverse;
 
 namespace Algorithms
 {
@@ -11,33 +10,10 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            int n = Int32.Parse(System.Console.ReadLine()), value;
-            ListNode first = null, current = null, next;
+            int[] a = { 9, 5, 3, 3, 5, 5, 4, 1, 2, 8, 10 };
 
-            for (int i = 0; i < n; i++)
-            {
-                value = Int32.Parse(System.Console.ReadLine());
-                next = new ListNode() { Value = value };
-
-                if (current == null)
-                {
-                    first = current = next;
-                }
-                else
-                {
-                    current.Next = next;
-                    current = next;
-                }
-            }
-
-            MustReverse.MustReverse.reverse(ref first);
-            current = first;
-
-            while (current != null)
-            {
-                Console.Write("{0} ", current.Value);
-                current = current.Next;
-            }
+            System.Console.WriteLine(Fun.Pond.Pond.MaxDepth(a));
+            System.Console.WriteLine(Fun.Pond.Pond.Volume(a));
         }
     }
 }
