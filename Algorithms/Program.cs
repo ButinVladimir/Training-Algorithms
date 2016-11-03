660dc98953c9fc65bd33a3adf6ef2a6471ca5a8a
Vladimir Butin <vladimir.butin@akvelon.com>
2016-11-03 16:44:35 +0300

    Factorizations task from set 47

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -10,68 +10,26 @@ namespace Algorithms
 
     class Program
     {
-        static private void Push(MinStack stack, int val)
+        static void PrintList(LinkedList <int> list)
         {
-            Console.WriteLine("Before");
-            Output(stack);
-            Console.WriteLine("");
-
-            stack.Push(val);
-            Console.WriteLine("Pushed {0}", val);
-
-            Console.WriteLine("\nAfter");
-            Output(stack);
-            Console.WriteLine("----------------------------------------------------------------------------------------");
-        }
-
-        static private void Pop(MinStack stack)
-        {
-            Console.WriteLine("Before");
-            Output(stack);
-            Console.WriteLine("");
-
-            stack.Pop();
-            Console.WriteLine("Popped");
-
-            Console.WriteLine("\nAfter");
-            Output(stack);
-            Console.WriteLine("----------------------------------------------------------------------------------------");
-        }
-
-        static private void Output(MinStack stack)
-        {
-            int value, min;
-            bool success;
-            success = stack.TryGetMin(out min);
-
-            if (!success)
+            foreach (int value in list)
             {
-                Console.WriteLine("Stack is empty");
-                return;
+                Console.Write("{0} ", value);
             }
-
-            success = stack.TryGetValue(out value);
-
-            Console.WriteLine("Value = {0}, Min = {1}", value, min);
+            Console.WriteLine();
         }
 
         static void Main(string[] args)
         {
-            MinStack stack = new MinStack();
+            Factorizations factorizations = new Factorizations();
 
-            Push(stack, -10);
-            Push(stack, 3);
-            Push(stack, 1);
-            Push(stack, 2);
-            Pop(stack);
-            Pop(stack);
-            Pop(stack);
-            Pop(stack);
-            Push(stack, -1);
-            Pop(stack);
-            Pop(stack);
-            Pop(stack);
-            Pop(stack);
+            int n = Convert.ToInt32(Console.ReadLine());
+            LinkedList <LinkedList <int> > result = factorizations.Solve(n);
+
+            foreach (LinkedList<int> list in result)
+            {
+                PrintList(list);
+            }
         }
     }
 }
---
+++ Algorithms/Set 47/Factorizations.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Set_47
+{
+    class Factorizations
+    {
+        private LinkedList<int> currentDivisors;
+        private LinkedList<LinkedList<int>> result;
+
+        private void AddToResult()
+        {
+            LinkedList<int> buffer = new LinkedList<int>();
+
+            foreach (int value in this.currentDivisors)
+            {
+                buffer.AddLast(value);
+            }
+
+            result.AddLast(buffer);
+        }
+
+        private void ProcessNumber(int number, int minDivisor = 2)
+        {
+            if (number == 1)
+            {
+                this.AddToResult();
+            }
+
+            for (int divisor = minDivisor; divisor <= number && divisor * divisor <= number; divisor++)
+            {
+                if (number % divisor == 0)
+                {
+                    currentDivisors.AddLast(divisor);
+                    this.ProcessNumber(number / divisor, divisor);
+                    currentDivisors.RemoveLast();
+                }
+            }
+
+            if (number >= minDivisor)
+            {
+                currentDivisors.AddLast(number);
+                this.ProcessNumber(1, number);
+                currentDivisors.RemoveLast();
+            }
+        }
+
+        public LinkedList<LinkedList<int>> Solve(int number)
+        {
+            this.result = new LinkedList<LinkedList<int>>();
+            this.currentDivisors = new LinkedList<int>();
+
+            this.ProcessNumber(number);
+
+            return result;
+        }
+    }
+}
+
+
+/*
+ * 
+ * Usage
+ 
+        static void PrintList(LinkedList <int> list)
+        {
+            foreach (int value in list)
+            {
+                Console.Write("{0} ", value);
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Factorizations factorizations = new Factorizations();
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            LinkedList <LinkedList <int> > result = factorizations.Solve(n);
+
+            foreach (LinkedList<int> list in result)
+            {
+                PrintList(list);
+            }
+        }
+
+ */
