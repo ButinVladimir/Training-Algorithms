5449eb263b7f4b6c345460baf935868821f71d73
Vladimir Butin <vladimir.olre.butin@gmail.com>
2017-02-02 16:27:36 +0300

    Tree task from set 65

--- Algorithms/Set 65/Tree.cs
+++ Algorithms/Set 65/Tree.cs
@@ -1,101 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
-//using Algorithms.Set_65;
-
-public class Program
+namespace Algorithms.Set_65
 {
-    private class Tokenizer
-    {
-        private string currentString = null;
-
-        private string[] tokens = null;
-
-        private int tokenNumber = 0;
-
-        private static readonly char[] Separators = { ' ' };
-
-        public T NextToken<T>(Func<string, T> parser)
-        {
-            return parser(this.GetNextToken());
-        }
-
-        public string NextToken()
-        {
-            return this.GetNextToken();
-        }
-
-        public int NextInt()
-        {
-            return this.NextToken(int.Parse);
-        }
-
-        public long NextLong()
-        {
-            return this.NextToken(long.Parse);
-        }
-
-        private string GetNextToken()
-        {
-            if (this.currentString == null || this.tokenNumber == this.tokens.Length)
-            {
-                this.currentString = this.GetNextString();
-
-                while (this.currentString != null && this.currentString.Equals(string.Empty))
-                {
-                    this.currentString = this.GetNextString();
-                }
-
-                if (this.currentString == null)
-                {
-                    throw new Exception("End of input");
-                }
-
-                this.tokens = this.currentString.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
-                this.tokenNumber = 0;
-            }
-
-            return this.tokens[this.tokenNumber++];
-        }
-
-        private string GetNextString()
-        {
-            string content = Console.ReadLine();
-            if (content == null)
-            {
-                return null;
-            }
-
-            return content.Trim();
-        }
-    }
-
-    public static void Main()
-    {
-        //Console.SetIn(new StreamReader(File.OpenRead("input.txt")));
-        //StreamWriter writer = new StreamWriter(File.Create("output.txt"));
-        //Console.SetOut(writer);
-
-        Tokenizer tokenizer = new Tokenizer();
-
-        int n = tokenizer.NextInt();
-        Tree tree = new Tree(n);
-
-        for (int i = 0; i < n - 1; i++)
-        {
-            tree.AddRib(
-                tokenizer.NextInt() - 1, 
-                tokenizer.NextInt() - 1, 
-                tokenizer.NextToken()[0] == 'r');
-        }
-
-        Console.WriteLine(tree.Solve());
-
-        //writer.Close();
-    }
+    using System.Net;
 
     public class Tree
     {
@@ -292,7 +203,7 @@ public class Program
                 }
             }
 
-            return result;
+            return result;            
         }
 
         private class TreeNode
