4d128c1d76525eede154df1b2b6a479e9282e6a4
Vladimir Butin <vladimir.olre.butin@gmail.com>
2017-02-13 13:54:26 +0300

    Down to Zero and Queries with fixed length tasks from hackerrank
    Funsies!

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.IO;
 using System.Text;
 
-//using Algorithms.Set_67;
+//using Algorithms.Fun;
 
 class Solution
 {
@@ -81,7 +81,83 @@ class Solution
 
         Tokenizer tokenizer = new Tokenizer();
 
+        int n = tokenizer.NextInt();
+        int q = tokenizer.NextInt();
+
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = tokenizer.NextInt();
+        }
+
+        FixedLengthQueries flq = new FixedLengthQueries() { Numbers = a };
+        flq.Prepare();
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(flq.Answer(tokenizer.NextInt()));
+        }
 
         //writer.Close();
+    }
+
+    class FixedLengthQueries
+    {
+        private int[] result;
+
+        public int[] Numbers { get; set; }
+
+        public void Prepare()
+        {
+            int n = this.Numbers.Length;
+
+            int[] left = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                left[i] = i - 1;
+
+                while (left[i] >= 0 && this.Numbers[left[i]] <= this.Numbers[i])
+                {
+                    left[i] = left[left[i]];
+                }
+            }
+
+            int[] right = new int[n];
+            for (int i = n - 1; i >= 0; i--)
+            {
+                right[i] = i + 1;
+
+                while (right[i] < n && this.Numbers[right[i]] <= this.Numbers[i])
+                {
+                    right[i] = right[right[i]];
+                }
+            }
+
+            this.result = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                this.result[i] = int.MaxValue;
+            }
+
+            int d;
+
+            for (int i = 0; i < n; i++)
+            {
+                d = right[i] - left[i] - 2;
+                this.result[d] = Math.Min(this.result[d], this.Numbers[i]);
+            }
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                this.result[i] = Math.Min(this.result[i], this.result[i + 1]);
+            }
+        }
+
+        public int Answer(int d)
+        {
+            return this.result[d - 1];
+        }
     }
 }
