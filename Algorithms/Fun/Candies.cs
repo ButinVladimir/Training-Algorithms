c4caf7386b3809177f29db7d195c93e0c0c30a48
Vladimir Butin <vladimir.olre.butin@gmail.com>
2017-02-19 21:22:59 +0300

    Candies task for fun

---
+++ Algorithms/Fun/Candies.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Fun
+{
+    public class Candies
+    {
+        public static long Solve(long[] rating)
+        {
+            int n = rating.Length;
+            long[] candies = new long[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                candies[i] = 1;
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                if (rating[i] > rating[i - 1])
+                {
+                    candies[i] = candies[i - 1] + 1;
+                }
+            }
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if (rating[i] > rating[i + 1])
+                {
+                    candies[i] = candies[i + 1] + 1;
+                }
+            }
+
+            long result = 0;
+            for (int i = 0; i < n; i++)
+            {
+                result += candies[i];
+            }
+
+            return result;
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.IO;
 using System.Text;
 
-//using Algorithms.Set_67;
+//using Algorithms.Fun;
 
 class Solution
 {
@@ -80,8 +80,53 @@ class Solution
         //Console.SetOut(writer);
 
         Tokenizer tokenizer = new Tokenizer();
+        int n = tokenizer.NextInt();
 
+        long[] rating = new long[n];
+        for (int i=0;i<n;i++)
+        {
+            rating[i] = tokenizer.NextLong();
+        }
 
+        Console.WriteLine(Candies.Solve(rating));
         //writer.Close();
+    }
+
+    public class Candies
+    {
+        public static long Solve(long[] rating)
+        {
+            int n = rating.Length;
+            long[] candies = new long[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                candies[i] = 1;
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                if (rating[i] > rating[i - 1])
+                {
+                    candies[i] = candies[i - 1] + 1;
+                }
+            }
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if (rating[i] > rating[i + 1])
+                {
+                    candies[i] = Math.Max(candies[i], candies[i + 1] + 1);
+                }
+            }
+
+            long result = 0;
+            for (int i = 0; i < n; i++)
+            {
+                result += candies[i];
+            }
+
+            return result;
+        }
     }
 }
