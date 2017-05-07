3629647dbd12b1c1a11b935446926c68523859f7
Vladimir Butin <vladimir.olre.butin@gmail.com>
2017-05-07 18:36:58 +0300

    Longest Subarray task from set 21

--- Algorithms/Set 21/LongestSubarray.cs
+++ Algorithms/Set 21/LongestSubarray.cs
@@ -12,6 +12,50 @@ namespace Algorithms.Set_21
         public static int Solve(int[] a)
         {
             int result = 0;
+            int nn = a.Length + 2;
+
+            int[] negativePositions = new int[nn];
+            for (int i = 0; i < nn; i++)
+            {
+                negativePositions[i] = -1;
+            }
+
+            int current = 0;
+            for (int position = 0; position < a.Length; position++)
+            {
+                if (a[position] == 0)
+                {
+                    current++;
+                }
+                else
+                {
+                    current--;
+                }
+
+                if (current > 0)
+                {
+                    result = Math.Max(result, position + 1);
+                }
+                else
+                {
+                    if (negativePositions[-current] == -1)
+                    {
+                        negativePositions[-current] = position;
+                    }
+
+                    if (negativePositions[-current + 1] != -1)
+                    {
+                        result = Math.Max(result, position - negativePositions[-current + 1]);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static int BruteSolve(int[] a)
+        {
+            int result = 0;
             int current;
 
             for (int start = 0; start < a.Length; start++)
@@ -46,28 +90,42 @@ namespace Algorithms.Set_21
         public void Test1()
         {
             int[] a = new int[] { 0, 0, 0, 0, 0, 0 };
-            Assert.AreEqual(6, LongestSubarray.Solve(a));
+            Assert.AreEqual(LongestSubarray.BruteSolve(a), LongestSubarray.Solve(a));
         }
 
         [TestMethod]
         public void Test2()
         {
             int[] a = new int[] { 0, 0, 1, 0, 0, 0 };
-            Assert.AreEqual(6, LongestSubarray.Solve(a));
+            Assert.AreEqual(LongestSubarray.BruteSolve(a), LongestSubarray.Solve(a));
         }
 
         [TestMethod]
         public void Test3()
         {
             int[] a = new int[] { 1, 1, 1, 0, 0, 0, 0 };
-            Assert.AreEqual(7, LongestSubarray.Solve(a));
+            Assert.AreEqual(LongestSubarray.BruteSolve(a), LongestSubarray.Solve(a));
         }
 
         [TestMethod]
         public void Test4()
         {
             int[] a = new int[] { 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0 };
-            Assert.AreEqual(7, LongestSubarray.Solve(a));
+            Assert.AreEqual(LongestSubarray.BruteSolve(a), LongestSubarray.Solve(a));
+        }
+
+        [TestMethod]
+        public void Test5()
+        {
+            int[] a = new int[] { 1, 1, 1, 1, 1, 1, 1 };
+            Assert.AreEqual(LongestSubarray.BruteSolve(a), LongestSubarray.Solve(a));
+        }
+
+        [TestMethod]
+        public void Test6()
+        {
+            int[] a = new int[] { 1, 0, 1, 0, 1, 0, 1 };
+            Assert.AreEqual(LongestSubarray.BruteSolve(a), LongestSubarray.Solve(a));
         }
     }
 }
