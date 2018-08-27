ec5c5f3351a0d1fa7f902420fb27be132e21bde0
Vladimir Butin <vladimir.olre.butin@gmail.com>
2018-08-27 15:14:54 +0300

    small task

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -108,83 +108,40 @@ public class Solution
 
         int n = tokenizer.NextInt();
         string s = tokenizer.NextToken();
-        Console.WriteLine(TwoCharacters.Solve(s));
+        Console.WriteLine(BeautifulBinaryString.Solve(s));
 
         //writer.Close();
         //}
         //}
     }
 
-    public static class TwoCharacters
+    public static class BeautifulBinaryString
     {
         public static int Solve(string s)
         {
+            int l = 0;
+            int r;
             int result = 0;
-
-            for (char c1 = 'a'; c1 <= 'z'; c1++)
+            while (l < s.Length)
             {
-                for (char c2 = 'a'; c2 <= 'z'; c2++)
+                if (s[l] == '1')
                 {
-                    if (c1 != c2)
-                    {
-                        result = Math.Max(result, SolveInternal(s, c1, c2));
-                    }
+                    l++;
+                    continue;
+                }
+
+                if (l < s.Length - 2 && s.Substring(l, 3) == "010")
+                {
+                    result++;
+                    l += 3;
+                }
+                else
+                {
+                    l++;
                 }
             }
 
             return result;
-        }
-
-        private static int SolveInternal(string s, char c1, char c2)
-        {
-            int p = 0;
-            while (p < s.Length && s[p] != c1)
-            {
-                if (s[p] == c2)
-                {
-                    return 0;
-                }
-                p++;
-            }
-            if (p == s.Length)
-            {
-                return 0;
-            }
-            p++;
-
-            int result = 1;
-            bool second = true;
-            while (p < s.Length)
-            {
-                if (second)
-                {
-                    if (s[p] == c2)
-                    {
-                        second = false;
-                        result++;
-                    }
-                    else if (s[p] == c1)
-                    {
-                        return 0;
-                    }
-                }
-                else
-                {
-                    if (s[p] == c1)
-                    {
-                        second = true;
-                        result++;
-                    }
-                    else if (s[p] == c2)
-                    {
-                        return 0;
-                    }
-                }
-
-                p++;
-            }
-
-            return result > 1 ? result : 0;
         }
     }
 }
